ba613b7d19cf0b456a4728f72856cf4137a6ffe2
Mário Becker <mario.roberto1@hotmail.com>
2023-06-03 12:52:41 -0300

    day 2

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
 
 import Groups from '@screens/Groups'
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import theme from '@theme/index';
+import { useFonts, Roboto_400Regular, Roboto_700Bold  } from '@expo-google-fonts/roboto';
+import { Loading } from '@components/Loading';
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
+
+  
   return (
     <ThemeProvider theme={theme}>
-        <Groups />
+        {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
 );
 }
